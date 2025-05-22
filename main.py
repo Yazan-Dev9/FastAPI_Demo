327042ff9714f48d495f29cc1c2b4d32cf905c73
Yazan-Dev9 <yksy.dev@gmail.com>
2025-05-23 00:17:45 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+list =[
+[1,"yazan", 25]
+]
+
 @app.get("/")
 def read_root():
     return {"message": "Hello YAZAN from Railway!"}
 
+@app.get("/{id}")
+def read_id(id: int):
+    for item in list:
+        if id in item:
+            return {"name": item[1], "age": item[2]}
+    return {"message" : "Error"}
