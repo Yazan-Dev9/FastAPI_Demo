7971810ac2ac4d6e6a7ae3ab5f6a23da4a463dad
Yazan-Dev9 <yksy.dev@gmail.com>
2025-05-23 01:21:39 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
 list =[
@@ -10,6 +10,14 @@ list =[
 def read_root():
     return {"message": "Hello YAZAN from Railway!"}
 
+@app.get("/all")
+def read_all():
+    result = []
+    for item in list:
+        dict = {"id": item[0],"name": item[1],"age": item[2]}
+        result.append(dict)
+    return result
+
 @app.get("/{id}")
 def read_id(id: int):
     for item in list:
@@ -18,7 +26,24 @@ def read_id(id: int):
     return {"message" : "Error"}
 
 @app.post("/")
-def post_item(id,name,age):
+def post_item(id: int ,name: str ,age: int):
     item = [id,name,age]
-    list.appind(item)
+    list.append(item)
     return {"message" : "Sccessfull"}
+
+@app.delete("/{id}")
+def delete_item(id: int):
+    for item in list:
+        if id in item:
+            list.remove(item)
+            return {"message" : "Sccessfull"}
+
+@app.put("/{id}")
+def update_item(id: int,name: Optional[str] = None,age: Optional[int] = None):
+    for item in list:
+        if id in item:
+            if name:
+                item[1]=name
+            if age:
+                item[2]=age
+            return {"message" : "Sccessfull"}
